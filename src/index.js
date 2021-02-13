e873449a3e3899b34585691fa22c5cebf907d4ba
Basil-Panasiuk <61430197+Basil-Panasiuk@users.noreply.github.com>
2021-02-13 16:59:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array)) {
+    if(array.length < 1) {
+      return 0;
+    }
+    let min = array[0];
+    for(let i = 1; i < array.length; i++) {
+      if(min > array[i]) {
+        min = array[i];
+      }
+    }
+    return min;
+  } else return 0;
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array)) {
+    if(array.length < 1) {
+      return 0;
+    }
+    let max = array[0];
+    for(let i = 1; i < array.length; i++) {
+      if(max < array[i]) {
+        max = array[i];
+      }
+    }
+    return max;
+  } else return 0;
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array)) {
+    if(array.length < 1) {
+      return 0;
+    }
+    let s = 0;
+  for(let i = 0; i < array.length; i++){
+    s += array[i];
+  }
+
+  return s/array.length;
+  } else return 0;
+
 }
+
